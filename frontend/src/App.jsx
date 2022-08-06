a4cb046d849970817decd44b01d2586b8c08bacf
hannahmarie9100 <110740182+hannahmarie9100@users.noreply.github.com>
2022-08-06 19:05:01 +0000

    Deploying to published from @ hannahmarie9100/tictactoe@85f3d5bb9343e8ddb5e114404a44111f9ce6cac4 🚀

---
+++ frontend/src/App.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { ThemeProvider, Typography, Stack, Box } from '@mui/material';
+
+import client, { events } from '@urturn/client';
+import theme from './theme';
+
+function App() {
+  const [boardGame, setBoardGame] = useState(client.getBoardGame() || {});
+  useEffect(() => {
+    const onStateChanged = (newBoardGame) => {
+      setBoardGame(newBoardGame);
+    };
+    events.on('stateChanged', onStateChanged);
+    return () => {
+      events.off('stateChanged', onStateChanged);
+    };
+  }, []);
+
+  console.log('boardGame:', boardGame);
+
+  const {
+    state: {
+      board
+    } = {
+      board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+      ]
+    }
+  } = boardGame;
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography>
+        <Stack margin={2} spacing={1} direction="row" justifyContent="center">
+          <Box>
+            {board.map((row, rowNum) => (
+              <Stack key={rowNum} direction="row">
+                {row.map((val, colNum) => (
+                  <Stack
+                    onClick={async (event) => {
+                      event.preventDefault();
+                      const move = { x: rowNum, y: colNum };
+                      await client.makeMove(move);
+                    }}
+                    key={colNum}
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    sx={{
+                      border: 1,
+                      borderColor: 'text.primary',
+                      height: '100px',
+                      width: '100px',
+                    }}
+                  >
+                    <Typography color="text.primary" fontSize="60px">
+                      {val}
+                    </Typography>
+                  </Stack>
+                ))}
+              </Stack>
+            ))}
+          </Box>
+        </Stack>
+      </Typography>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,153 @@
+// TicTacToe Example
+/**
+ * Generic board game types
+ * @type Player: json object, in the format of
+ * {
+ *  id: string, unique player id
+ *  username: string, the player's display name
+ * }
+ * @type BoardGame: json object, in the format of
+ * {
+ *  // creator read write fields
+ *  state: json object, which represents any board game state
+ *  joinable: boolean (default=true), whether or not the room can have new players added to it
+ *  finished: boolean (default=false), when true there will be no new board game state changes
+ *
+ *  // creator read only
+ *  players: [Player], array of player objects
+ *  version: Number, an integer value that increases by 1 with each state change
+ * }
+ * @type BoardGameResult: json object, in the format of
+ * {
+ *  // fields that creator wants to overwrite
+ *  state?: json object, which represents any board game state
+ *  joinable?: boolean, whether or not the room can have new players added to it
+ *  finished?: boolean, when true there will be no new board game state changes
+ * }
+ */
+
+/**
+ * onRoomStart
+ * @returns {BoardGameResult}
+ */
+function onRoomStart() {
+  return {
+    state: {
+      board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+      ],
+      winner: null
+    }
+  };
+}
+
+/**
+ * onPlayerJoin
+ * @param {Player} player, represents the player that is attempting to join this game
+ * @param {BoardGame} currentGame
+ * @returns {BoardGameResult}
+ */
+function onPlayerJoin(plr, boardGame) {
+  const { players } = boardGame;
+
+  if (players.length === 2) {
+    return { joinable: false };
+  }
+  return {};
+}
+
+/**
+ * onPlayerMove
+ * @param {Player} player, the player that is attempting to make a move
+ * @param {*} move json object, controlled the creator that represents the player's move
+ * @param {BoardGame} currentGame
+ * @returns {BoardGameResult}
+ */
+function getPlrMark(plr, plrs) {
+  if (plr === plrs[0]) { // for simplicity, the first player will be 'X'
+    return 'X';
+  }
+  return 'O';
+}
+
+function isEndGame(board, plrs) {
+  function getPlrFromMark(mark, plrs) {
+    return mark === 'X' ? plrs[0] : plrs[1];
+  }
+
+  function isWinningSequence(arr) {
+    return arr[0] !== null && arr[0] === arr[1] && arr[1] === arr[2];
+  }
+
+  // check rows and cols
+  for (let i = 0; i < board.length; i += 1) {
+    const row = board[i];
+    const col = [board[0][i], board[1][i], board[2][i]];
+
+    if (isWinningSequence(row)) {
+      return [true, getPlrFromMark(row[0], plrs)];
+    } if (isWinningSequence(col)) {
+      return [true, getPlrFromMark(col[0], plrs)];
+    }
+  }
+
+  // check diagonals
+  const d1 = [board[0][0], board[1][1], board[2][2]];
+  const d2 = [board[0][2], board[1][1], board[2][0]];
+  if (isWinningSequence(d1)) {
+    return [true, getPlrFromMark(d1[0], plrs)];
+  } if (isWinningSequence(d2)) {
+    return [true, getPlrFromMark(d2[0], plrs)];
+  }
+
+  // check for tie
+  if (board.some((row) => row.some((mark) => mark === null))) {
+    return [false, null];
+  }
+  return [true, null];
+}
+
+function onPlayerMove(plr, move, boardGame) {
+  const { state, players } = boardGame;
+  const { board, plrToMoveIndex } = state;
+
+  const { x, y } = move;
+
+  const plrMark = getPlrMark(plr, players);
+
+  board[x][y] = plrMark;
+
+  const [isEnd, winner] = isEndGame(board, players);
+
+  if (isEnd) {
+    state.winner = winner;
+    return { state, finished: true };
+  }
+  return { state };
+}
+
+/**
+ * onPlayerQuit
+ * @param {Player} player, the player that is attempting to quit the game
+ * @param {BoardGame} currentGame
+ * @returns {BoardGameResult}
+ */
+function onPlayerQuit(plr, boardGame) {
+  const { state, players } = boardGame;
+
+  if (players.length === 1) {
+    const [winner] = players;
+    state.winner = winner;
+    return { state, joinable: false, finished: true };
+  }
+  return { joinable: false, finished: true };
+}
+
+module.exports = {
+  onRoomStart,
+  onPlayerJoin,
+  onPlayerMove,
+  onPlayerQuit,
+};
