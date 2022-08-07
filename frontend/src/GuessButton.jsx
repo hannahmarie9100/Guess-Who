608040fccd773d03e1ae95ecc46ee82c10f2135f
hannah <hma0910@gmail.com>
2022-08-07 15:08:54 -0500

    guess button writing

--- frontend/src/GuessButton.jsx
+++ frontend/src/GuessButton.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
 
 function GuessButton() {
-    return <Box
+    return <Button
         sx={{
             width: 100,
             height: 50,
             backgroundColor: "#69B7FF",
-            margin: "15px"
+            margin: "15px",
+            color: "white"
         }}
-    >
-    </Box>
+    >Guess
+    </Button>
 }
 
 export default GuessButton
