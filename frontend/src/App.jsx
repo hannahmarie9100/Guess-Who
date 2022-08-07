76a8de952d69c957d5f181b76e903c3b1d03667e
sarahannali <55365172+sarahannali@users.noreply.github.com>
2022-08-07 22:15:53 +0000

    Deploying to published from @ hannahmarie9100/tictactoe@e522a11b6d309ae968a5a12dc9e7dd52f85d855d 🚀

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
 
   useEffect(() => {
     const onStateChanged = (newBoardGame) => {
+      console.log("NEW BOARD GAME: ", newBoardGame)
       setBoardGame(newBoardGame);
     };
     const getLocalPlayer = async () => {
